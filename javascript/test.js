089cd3f9e6235487442c22e704db4ef2a2e19088
steven-leonardo <stevenleonardolim@gmail.com>
2023-04-30 21:57:59 +0700

    first

---
+++ javascript/test.js
@@ -0,0 +1,79 @@
+// 1. kamu ingin memakan mie di malam hari, dengan memakan mie ini akan membuat kamu kenyang dan bertahan hingga esok hari
+// coba buatkan pseudocode dari cara kamu membuar mie itu hingga di hidangkan.
+
+// 1. buka bungkusan mie 
+// 2. rebus air hingga mendidih
+// 3. masukkan mie ke dalam air mendidih
+// 4. buka dan tuang bumbu mie ke dalam piring 
+// 5. angat dan tiriskan mie 
+// 6. aduk mie hingga bumbu tercampur rata 
+// 7. mie siap dihidangkan
+
+/*budi ingin membantu ibu, dengan hati yang senang ibu menyuruh budi untuk mencuci pakaian sekeluarga, pada saat itu
+ada sejumlah 60 baju di lantai yang tertata karena belm dirapikan, maka tugas si budi adalah untuk membersihkan
+pakaian itu hingga siap ducuci. */
+// const baju_keluarga= 60
+// let keranjang= 0
+// let mesin_cuci= 0
+
+
+// function masukkan_keranjang(baju){
+// keranjang= baju
+// console.log("KERANJANG =",keranjang);
+// }
+
+// function masukkan_mesincuci(keranjang){
+// mesin_cuci=keranjang
+// console.log("mesincuci=",mesin_cuci)
+// }
+
+// function mesincuci(){
+//     console.log(`sejumlah ${mesin_cuci} pakaian sudah bersih`)
+// }
+// masukkan_keranjang(baju_keluarga)
+// masukkan_mesincuci(keranjang)
+// mesincuci()
+
+
+// 1. budi ingin membantu ibu mencuci pakaian sekeluarga
+// 2. budi mencuci 60 baju yang belum dirapikan 
+// 3. sejumlah 60 baju sekeluarga siap dicuci budi
+
+// 1. ada satu keluarga beranggotakan 6 orang jadi buat variable dulu untuk nampung keluarga dengan variable bernama keluarga
+// 2. setiap orang menginginkan 1 mie dan 1 telur jadi ada 6 orang sehingga buat variable bernama telur dan mie berjumlah 6
+// 3. ketika dimasak kan maka mie dierbus dahulu berbaregan dengan telur secara bersamaan
+// 4. mie matang dan siap dihidangkan untuk 6 orang
+
+
+// buatlah suatu fungsi untuk mencari nilai dari gaya, dengam massa berjumlah 70 yang dengan variable m dan percepatan
+// sejumlah 30 dengan variable a, carilah jumlah dari gaya tersebut dengan variable F
+
+// let m= 70
+// let a= 30
+// let F= 0
+
+// function gaya(z,j){
+// F=z*j
+// console.log(F);
+// }
+
+// gaya(90,100)
+
+//ada seorang anak mendapatkan nilai 70 benar dari 100 soal dan 1 soal benar itu bernilai 1, dan nilai aslinya itu adalah
+//nilai dari anak itu dibagi 2 coba cari nilai anak tersebut
+
+// let n= 70
+
+// function nilai(n){
+//     console.log(n/2);
+// }
+
+// nilai(70)
+
+// sebulan ada 30 hari setiap hari ganjil ada upacara bendera coba cari nilai berapa kali upacara bendera yang dilakukan selama 30 hari tersebut
+
+function hari(h){
+    console.log(h/2);
+}
+
+hari(30)
